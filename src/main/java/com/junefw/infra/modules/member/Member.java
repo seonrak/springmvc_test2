ff99cb78833f3a4c5d98408f570669eeb0ecbb05
seonrak <six_find@naver.com>
2022-02-28 17:28:12 +0900

    20220228

---
+++ src/main/java/com/junefw/infra/modules/member/Member.java
@@ -0,0 +1,34 @@
+package com.junefw.infra.modules.member;
+
+public class Member {
+	
+	private String ifmmSeq;
+	private String ifmmId;
+	private String ifmmName;
+	private Integer ifmmDelNy;
+//------------
+	public String getIfmmSeq() {
+		return ifmmSeq;
+	}
+	public void setIfmmSeq(String ifmmSeq) {
+		this.ifmmSeq = ifmmSeq;
+	}
+	public String getIfmmId() {
+		return ifmmId;
+	}
+	public void setIfmmId(String ifmmId) {
+		this.ifmmId = ifmmId;
+	}
+	public String getIfmmName() {
+		return ifmmName;
+	}
+	public void setIfmmName(String ifmmName) {
+		this.ifmmName = ifmmName;
+	}
+	public Integer getIfmmDelNy() {
+		return ifmmDelNy;
+	}
+	public void setIfmmDelNy(Integer ifmmDelNy) {
+		this.ifmmDelNy = ifmmDelNy;
+	}
+}
